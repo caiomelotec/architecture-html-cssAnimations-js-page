c217be7e0ec21901fdcd3d593453bad37bc597e8
caiomelotec <caio.melotec@icloud.com>
2023-05-23 13:22:39 +0200

    update

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".next",
     prevEl: ".prev",
   },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    650: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
 });
